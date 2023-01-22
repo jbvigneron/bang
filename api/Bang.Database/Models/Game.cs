50576e84cb5af26106e151839ff731ee12159b60
Jean-Baptiste Vigneron <jb.vigneron@live.fr>
2023-01-22 18:02:10 +0100

    feat: initialize deck

--- api/Bang.Database/Models/Game.cs
+++ api/Bang.Database/Models/Game.cs
@@ -1,5 +1,4 @@
 ﻿using Bang.Database.Enums;
-using System.Text.Json.Serialization;
 
 namespace Bang.Database.Models
 {
@@ -10,9 +9,6 @@ namespace Bang.Database.Models
         public string? CurrentPlayerName { get; set; }
         public virtual IList<Player> Players { get; set; }
         public int DeckCount { get; set; }
-
-        [JsonIgnore]
-        public virtual ICollection<GameDeckCard> Deck { get; set; }
         public virtual IEnumerable<GameDiscardCard> DiscardPile { get; set; }
     }
 }
