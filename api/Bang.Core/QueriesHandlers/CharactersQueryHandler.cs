a4840a35a8ddfecacaf873bef96d8d8072537386
Jean-Baptiste Vigneron <jb.vigneron@live.fr>
2023-02-19 20:58:42 +0100

    feat: add Characters and Roles controllers

---
+++ api/Bang.Core/QueriesHandlers/CharactersQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Bang.Core.Queries;
+using Bang.Database;
+using Bang.Models;
+using MediatR;
+
+namespace Bang.Core.QueriesHandlers
+{
+    public class CharactersQueryHandler : RequestHandler<CharactersQuery, IEnumerable<Character>>
+    {
+        private readonly BangDbContext dbContext;
+
+        public CharactersQueryHandler(BangDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        protected override IEnumerable<Character> Handle(CharactersQuery request)
+        {
+            return this.dbContext.Characters;
+        }
+    }
+}
