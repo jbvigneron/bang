987d50b79b27654dec68f5e2124567da501f4463
Jean-Baptiste Vigneron <jb.vigneron@live.fr>
2023-02-20 23:09:29 +0100

    chore: add Serilog

--- api/Bang.WebApi/Program.cs
+++ api/Bang.WebApi/Program.cs
@@ -2,74 +2,114 @@ using Bang.Core;
 using Bang.Core.Hubs;
 using Bang.Database;
 using Bang.WebApi.Middlewares;
+using Hellang.Middleware.ProblemDetails;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Exceptions;
 using System.Reflection;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.File("log.txt")
+    .Enrich.FromLogContext()
+    .Enrich.WithCorrelationId()
+    .Enrich.WithExceptionDetails()
+    .Enrich.WithDemystifiedStackTraces()
+    .CreateLogger();
 
-builder.Services.AddControllers();
+try
+{
+    Log.Information("Starting web application");
 
-builder.Services.AddDbContext<BangDbContext>(options =>
-               options.UseInMemoryDatabase("BangDb")
-);
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.RegisterCore();
+    builder.Host.UseSerilog();
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
+    builder.Services.AddControllers();
+
+    builder.Services.AddProblemDetails(opts =>
     {
-        options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
-        options.SlidingExpiration = true;
-        options.EventsType = typeof(CustomCookieAuthenticationEvents);
+        opts.IncludeExceptionDetails = (context, ex) =>
+        {
+            var environment = context.RequestServices.GetRequiredService<IHostEnvironment>();
+            return environment.IsDevelopment();
+        };
     });
 
-builder.Services.AddScoped<CustomCookieAuthenticationEvents>();
+    builder.Services.AddDbContext<BangDbContext>(options =>
+                   options.UseInMemoryDatabase("BangDb")
+    );
 
-builder.Services.AddSignalR();
+    builder.Services.RegisterCore();
 
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Bang API v1", Version = "v1" });
+    builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+            options.SlidingExpiration = true;
+            options.EventsType = typeof(CustomCookieAuthenticationEvents);
+        });
 
-    options.AddSignalRSwaggerGen(options =>
+    builder.Services.AddScoped<CustomCookieAuthenticationEvents>();
+
+    builder.Services.AddSignalR();
+
+    builder.Services.AddSwaggerGen(options =>
     {
-        var assemblyCore = Assembly.Load("Bang.Core");
-        options.ScanAssembly(assemblyCore);
+        options.SwaggerDoc("v1", new OpenApiInfo { Title = "Bang API v1", Version = "v1" });
+
+        options.AddSignalRSwaggerGen(options =>
+        {
+            var assemblyCore = Assembly.Load("Bang.Core");
+            options.ScanAssembly(assemblyCore);
+        });
+
+        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
     });
 
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
+    var app = builder.Build();
 
-var app = builder.Build();
+    app.UseProblemDetails();
+    app.UseSerilogRequestLogging();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+    else
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseRouting();
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.MapHub<PublicHub>("/PublicHub");
+    app.MapHub<GameHub>("/GameHub");
+    app.MapHub<PlayerHub>("/PlayerHub");
+
+    app.Run();
 }
-else
+catch (Exception ex)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Fatal(ex, "Application terminated unexpectedly");
 }
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapHub<PublicHub>("/PublicHub");
-app.MapHub<GameHub>("/GameHub");
-app.MapHub<PlayerHub>("/PlayerHub");
-
-app.Run();
+finally
+{
+    Log.CloseAndFlush();
+}
 
 public partial class Program { }
