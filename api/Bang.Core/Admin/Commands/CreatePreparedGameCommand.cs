e138081310d596a715891113ce300cceb12920ca
Jean-Baptiste Vigneron <jb.vigneron@live.fr>
2023-02-25 21:59:52 +0100

    feat: add Admin controller and associated commands

---
+++ api/Bang.Core/Admin/Commands/CreatePreparedGameCommand.cs
@@ -0,0 +1,15 @@
+﻿using Bang.Core.Admin.Models;
+using MediatR;
+
+namespace Bang.Core.Admin.Commands
+{
+    public class CreatePreparedGameCommand : IRequest<Guid>
+    {
+        public CreatePreparedGameCommand(IEnumerable<PlayersInfos> players)
+        {
+            this.Players = players;
+        }
+
+        public IEnumerable<PlayersInfos>? Players { get; set; }
+    }
+}
