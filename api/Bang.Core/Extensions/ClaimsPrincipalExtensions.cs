0092388dde2caf78a15d603f603ae3d33ec0ad7b
Jean-Baptiste Vigneron <jb.vigneron@live.fr>
2023-02-25 21:36:53 +0100

    refactor(auth): write JWT in cookie

---
+++ api/Bang.Core/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Bang.Core.Constants;
+using System.Security.Claims;
+
+namespace Bang.Core.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid GetId(this ClaimsPrincipal user) =>
+            Guid.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier));
+
+        public static string GetName(this ClaimsPrincipal user) =>
+            user.FindFirstValue(ClaimTypes.Name);
+
+        public static Guid GetGameId(this ClaimsPrincipal user) =>
+            Guid.Parse(user.FindFirstValue(JwtConstants.GameId));
+    }
+}
